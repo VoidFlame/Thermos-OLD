a057b50ad486841c579265967523e663e3b19438
kmecpp <kmecpp@gmail.com>
2019-01-12 14:19:45 -0500

    Revert "Regenerate patches"
    This reverts commit 57f0e08f2c43d77beea40be83e4bf7ce0e927c38.

--- makepatch.sh
+++ makepatch.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 for f in $(find eclipse/Clean -name '*.java'); do 
-	srcdir="${f:14:${#f}}"
-	clean="eclipse/Clean/$srcdir"
-	modified="eclipse/Cauldron/$srcdir"
+	dir="${f:14:${#f}}"
+	clean="eclipse/Clean/$dir"
+	modified="eclipse/Cauldron/$dir"
 	DIFF=$(diff -u $clean $modified) 
 	#DIFF=$(git diff --minimal --no-prefix --ignore-space-at-eol --ignore-blank-lines --no-index $clean $modified)
 	if [ "$DIFF" != "" ] 
 	then
-		dir="${f:28:${#f}}"
-		PATCH="patches/$dir.patch"
+		PATCH="patches/test/$dir.patch"
 		echo "Generating $PATCH"
-		mkdir -p "patches/$(dirname "$dir")"
+		mkdir -p "patches/test/$(dirname "$dir")"
 		echo "$DIFF" > $PATCH
 		#java makepatch $PATCH
 	fi
