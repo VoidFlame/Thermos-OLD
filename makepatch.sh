57f0e08f2c43d77beea40be83e4bf7ce0e927c38
kmecpp <kmecpp@gmail.com>
2019-01-12 14:15:53 -0500

    Regenerate patches

--- makepatch.sh
+++ makepatch.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 for f in $(find eclipse/Clean -name '*.java'); do 
-	dir="${f:14:${#f}}"
-	clean="eclipse/Clean/$dir"
-	modified="eclipse/Cauldron/$dir"
+	srcdir="${f:14:${#f}}"
+	clean="eclipse/Clean/$srcdir"
+	modified="eclipse/Cauldron/$srcdir"
 	DIFF=$(diff -u $clean $modified) 
 	#DIFF=$(git diff --minimal --no-prefix --ignore-space-at-eol --ignore-blank-lines --no-index $clean $modified)
 	if [ "$DIFF" != "" ] 
 	then
-		PATCH="patches/test/$dir.patch"
+		dir="${f:28:${#f}}"
+		PATCH="patches/$dir.patch"
 		echo "Generating $PATCH"
-		mkdir -p "patches/test/$(dirname "$dir")"
+		mkdir -p "patches/$(dirname "$dir")"
 		echo "$DIFF" > $PATCH
 		#java makepatch $PATCH
 	fi
